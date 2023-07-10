a36dd2936ebcaa1a0fc137fc012206b22be65591
Ernest <ernestwinata@gmail.com>
2023-07-09 20:25:10 -0700

    initial commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    sequence = []
+
+    if length <= 0:
+        print(sequence)
+        return
+    
+    sequence = [0, 1] if length >=2 else [0]
+    
+    while len(sequence) < length:
+        fibonacci_sequence = sequence[-1] + sequence[-2]
+        sequence.append(fibonacci_sequence)
+
+    print(sequence)
